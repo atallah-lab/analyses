aea2246af52a3e89b72b348fb22c4f07d885e5a1
Jack LeBien <jglebien@gmail.com>
2020-01-17 11:25:04 -0600

    update site annotation

--- map_genome.r
+++ map_genome.r
@@ -1,7 +1,5 @@
-#!/usr/bin/env Rscript
-
 # -----------------------------------------------------------------------------
-# mapgenome.r
+# map_genome.r
 
 # Maps potential insertion sites based on the 'Snap-Velcro' model described 
 # in Clement Monot, et al. (2013) "The Specificity and Flexibility of L1 Reverse 
@@ -11,8 +9,8 @@
 # locations of each type of EN site for both sense and anti-sense strands.
 # Locations and scores of the putative EN sites are initially stored in memory. 
 #
-# Usage: ./mapgen.r <start chromosome # (e.g. 1)> <end chromosome # (e.g. 24)>
-# Output: <chromosome name>map.rda to data directory file specified on line 123.
+# Usage: ./mapgenome.r <start chromosome # (e.g. 1)> <end chromosome # (e.g. 24)>
+# Output: <chromosome name>map.rda to data directory file specified on line 129.
 #
 # Dependencies: R(>= 2.8.0, Packages - Biostrings, BSgenome (for default hg38), 
 # GenomicRanges)
@@ -43,10 +41,12 @@ for (chrnm in names(genome)[start:end]) {
 	cat("Creating EN site annotation: sense...\n")
 
 	#--- 5' to 3' target sequence 'TTTT' (derived from Monot, et. al)
-	tar <- DNAString("TTTT")
+	tar <- DNAString("TTTTA")
 	#--- Finds target starting location in chromosome, one mismatch is allowed
 	mtchView <- matchPattern(tar,genome[[chrnm]],max.mismatch=1)
-
+    purines <- DNAStringSet(genome[[chrnm]],end(mtchView), end(mtchView))
+    mtchView <- mtchView[purines=='A'] # Doesn't allow mismatches for the purine
+    
 	#--- Calculates position weighted T-density scores
 	primrngs <- IRanges(start=start(mtchView)-6,width=6) # start and end points of 6 bp upstream of targets
 	targets <- DNAStringSet(genome[[chrnm]],start(mtchView),end(mtchView)) # collect targets from genome
@@ -58,28 +58,30 @@ for (chrnm in names(genome)[start:end]) {
 	primrnks <- lapply(tmp,function(x) sum(1/(11-x))/0.84563492) 
 	rm(tmp) # cleans up
 
-        #--- Stores indices of sites for each target category (from Monot, et. al).
-        #--- Example: 'ict_s' is site for 'closed' and 'tight' insertion.
-        ict_s<-which(targets==tar & primrnks >= 0.5)
-        icl_s<-which(targets==tar & primrnks < 0.5)
-        iot_s<-which(targets!=tar & primrnks >= 0.5)
-        iol_s<-which(targets!=tar & primrnks < 0.5)
-        insites_s<-end(mtchView)
+    #--- Stores indices of sites for each target category (from Monot, et. al).
+    #--- Example: 'ict_s' is site for 'closed' and 'tight' insertion.
+    ict_s<-which(targets==tar & primrnks >= 0.5)
+    icl_s<-which(targets==tar & primrnks < 0.5)
+    iot_s<-which(targets!=tar & primrnks >= 0.5)
+    iol_s<-which(targets!=tar & primrnks < 0.5)
+    insites_s<-end(mtchView)
 
 	### Anti-sense strand analysis
 	###------------------------------------------------------------------------###
 	cat("Creating EN site annotation: anti-sense...\n")
 
 	#--- 3' to 5' anti-sense target sequence 'AAAA' (derived from Monot, et. al)
-	tar <- DNAString("AAAA")
+	tar <- DNAString("TAAAA")
 	mtchView <- matchPattern(tar,genome[[chrnm]],max.mismatch=1)
-	primrngs <- IRanges(start=end(mtchView)+1,width=6)
+    purines <- DNAStringSet(genome[[chrnm]],start(mtchView), start(mtchView))
+    mtchView <- mtchView[purines=='T']
+    primrngs <- IRanges(start=end(mtchView)+1,width=6)
 	targets <- DNAStringSet(genome[[chrnm]],start(mtchView),end(mtchView))
-        prmrs <- DNAStringSet(genome[[chrnm]],start(primrngs),end(primrngs))
-        tmp <- vmatchPattern("A",prmrs)
-        tmp <- startIndex(tmp)
-        primrnks <- lapply(tmp,function(x) sum(1/(x+4))/0.84563492)
-        rm(tmp)
+    prmrs <- DNAStringSet(genome[[chrnm]],start(primrngs),end(primrngs))
+    tmp <- vmatchPattern("A",prmrs)
+    tmp <- startIndex(tmp)
+    primrnks <- lapply(tmp,function(x) sum(1/(x+4))/0.84563492)
+    rm(tmp)
 
 	ict_ns<-which(targets==tar & primrnks >= 0.5)
 	icl_ns<-which(targets==tar & primrnks < 0.5)
@@ -94,18 +96,18 @@ for (chrnm in names(genome)[start:end]) {
 	n <- max(length(ict_s), length(ict_ns))
 	length(ict_s) <- n                      
 	length(ict_ns) <- n
-        n <- max(length(icl_s), length(icl_ns))
+    n <- max(length(icl_s), length(icl_ns))
 	length(icl_s) <- n
-        length(icl_ns) <- n
-        n <- max(length(iot_s), length(iot_ns))
-        length(iot_s) <- n
-        length(iot_ns) <- n
-        n <- max(length(iol_s), length(iol_ns))
-        length(iol_s) <- n
-        length(iol_ns) <- n	
-        n <- max(length(insites_s), length(insites_ns))
-        length(insites_s) <- n
-        length(insites_ns) <- n
+    length(icl_ns) <- n
+    n <- max(length(iot_s), length(iot_ns))
+    length(iot_s) <- n
+    length(iot_ns) <- n
+    n <- max(length(iol_s), length(iol_ns))
+    length(iol_s) <- n
+    length(iol_ns) <- n	
+    n <- max(length(insites_s), length(insites_ns))
+    length(insites_s) <- n
+    length(insites_ns) <- n
 	#--- Bind the arrays for both strands as two columns
 	ict<-cbind(ict_s,ict_ns)
 	icl<-cbind(icl_s,icl_ns)
@@ -120,12 +122,12 @@ for (chrnm in names(genome)[start:end]) {
 	map$iol<-iol
 
 	#--- Save map under name of current chromosome
-	assign(paste0(chrnm,"Map"),map) # 'closed' and 'tight'.
+	assign(paste0(chrnm,"Map"),map)
 
 	cat("Saving map file...\n")
 	wd <- getwd()
-	save(list=(paste0(chrnm,"Map")),file=paste(wd, "./data/large_files/",chrnm,".rda",sep=""))
+	save(list=(paste0(chrnm,"Map")),file=paste(wd, "./data/l1_site_maps/",chrnm,".rda",sep=""))
 	rm(list=c("insites","ict","icl","iot","iol"))
 	end.time <- Sys.time()
-        print(end.time-start.time)
+    print(end.time-start.time)
 }
